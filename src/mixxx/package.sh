519f06fc0431adb0edfb0b76ba70415edec26c08
Carl Kittelberger <icedream@icedream.pw>
2019-03-07 04:27:14 +0100

    mixxx: Fix faulty installation.

--- src/mixxx/package.sh
+++ src/mixxx/package.sh
@@ -29,8 +29,8 @@ field architecture.\"64bit\".hash ""
 field checkver.url "https://mixxx.org/download/"
 field checkver.re '\bhttps://downloads.mixxx.org/mixxx-([\d.]+)\b'
 field autoupdate.hash.url "https://downloads.mixxx.org/mixxx-\$version/mixxx-\$version.sha256sum"
-field 'autoupdate.architecture."32bit".url' "https://downloads.mixxx.org/mixxx-\$version/mixxx-\$version-win32.exe"
-field 'autoupdate.architecture."64bit".url' "https://downloads.mixxx.org/mixxx-\$version/mixxx-\$version-win64.exe"
+field 'autoupdate.architecture."32bit".url' "https://downloads.mixxx.org/mixxx-\$version/mixxx-\$version-win32.exe#!/_setup.exe"
+field 'autoupdate.architecture."64bit".url' "https://downloads.mixxx.org/mixxx-\$version/mixxx-\$version-win64.exe#!/_setup.exe"
 #autofill_download
 
 #field 'architecture."32bit".hash' "$(find_sha256sum "mixxx-$version-win32.exe")"
@@ -40,7 +40,10 @@ field 'autoupdate.architecture."64bit".url' "https://downloads.mixxx.org/mixxx-\
 extra_file mixxx.mst
 
 pre_install <<-EOF
-	dark -nologo -sw -sct -sdet -sui -x "\$dir\\\\\$1" "\$dir\\\\setup.exe" NUL | Out-Null
-	$(ps_install_extra_file mixxx.mst "\$dir\\\\mixxx.mst")
+	dark -nologo -sw -sct -sdet -sui -x "\$dir\\\\\_1" "\$dir\\\\_setup.exe" NUL | Out-Null
+	rm "\$dir\\\\_setup.exe"
+	$(ps_install_extra_file mixxx.mst "\$dir\\\\_transform.mst")
 	run 'msiexec' @('/a', "\`"\$((get-item "\$dir\\\\_1\\\\AttachedContainer\\\\*.msi").FullName)\`"", '/qn', "TARGETDIR=\`"\$dir\`"", "TRANSFORMS=\`"\$dir\\\\_transform.mst\`"")
+	rm "\$dir\\\\*.*"
+	rm -Recurse "\$dir\\\\_1"
 EOF
