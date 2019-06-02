fc5406dcab6e41d47ad26783f8f64ec4279c5984
Carl Kittelberger <icedream@icedream.pw>
2019-06-02 21:43:32 +0200

    mixxx-nightly: Fix version detection for Windows binaries.
    This regex kept finding releases for which no Windows binaries were 
    built at all.

--- packages/mixxx-nightly/package.sh
+++ packages/mixxx-nightly/package.sh
@@ -5,7 +5,7 @@
 extend mixxx
 
 field checkver.url "https://downloads.mixxx.org/builds/master/release/"
-field checkver.re '\bmixxx\-([\w.\-]+\-git\d+)\-release\b'
+field checkver.re '\bmixxx\-([\w.\-]+\-git\d+)\-release-x86\.exe\b'
 remove_field 'autoupdate.hash'
 field 'autoupdate.architecture."32bit".url' "https://downloads.mixxx.org/builds/master/release/mixxx-\$version-release-x86.exe#!/_setup.exe"
 field 'autoupdate.architecture."64bit".url' "https://downloads.mixxx.org/builds/master/release/mixxx-\$version-release-x64.exe#!/_setup.exe"
